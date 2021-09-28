5ce8ed893d478c1f497561c0eabb5bd0a5662fac
student <student@158.197.35.210>
2021-09-28 12:12:23 +0200

    message

--- src/Prva.java
+++ src/Prva.java
@@ -2,7 +2,13 @@
 public class Prva {
 
 	public static void main(String[] args) {
+
 		System.out.println("Prvy vypis");
+
+		System.out.println("commit");
+		System.out.println("commit");
+		System.out.println("Autor: Git je hrozny");
+
 	}
 
 }
